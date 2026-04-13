9570a4cc984902c0a3b890de1afd13685bfe9a51
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2026-04-12 20:05:04 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
-export const metadata = {
-  title: "Phytocliniq",
-  description: "Evidence Made Clear",
-};
-
 import "./globals.css";
+
+export const metadata = {
+  title: "PHYTOCLINIQ",
+  description: "PHYTOCLINIQ",
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
+};
 
 export default function RootLayout({
   children,
